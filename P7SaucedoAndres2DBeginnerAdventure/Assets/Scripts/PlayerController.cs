3f99853867d8d88be62aaf0acf95df00bae5a308
Gogmazios <732861@student.dusd.net>
2025-11-14 15:27:22 -0800

    Moving
    The player can kind of move

--- P7SaucedoAndres2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
+++ P7SaucedoAndres2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
@@ -1,20 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem; 
 
 public class PlayerController : MonoBehaviour
 {
+    public InputAction LeftAction;
+    public InputAction MoveAction;
     // Start is called before the first frame update
     void Start()
     {
-        
+        LeftAction.Enable(); 
     }
 
     // Update is called once per frame
     void Update()
     {
+        float horizontal = 0.0f; 
+        float vertical = 0.0f;
+
+        if(LeftAction.IsPressed())
+        {
+            horizontal = -1.0f;
+        }
+        else if (Keyboard.current.rightArrowKey.isPressed)
+        {
+            horizontal = 1.0f; 
+        }
+        Debug.Log(horizontal);
+
+        if (Keyboard.current.upArrowKey.isPressed)
+        {
+            vertical = 1.0f;
+        }
+        else if ( Keyboard.current.downArrowKey.isPressed)
+        {
+            vertical = -1.0f;
+        }
+        Debug.Log(vertical);
+
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+        position.x = position.x + 5.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 5.0f * vertical * Time.deltaTime;
         transform.position = position; 
     }
 }
