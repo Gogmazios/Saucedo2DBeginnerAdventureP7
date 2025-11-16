ed8dfd63e8bf0a50babebc14c13ddb3d2a775562
Saucedo <732861@student.dusd.net>
2025-11-16 13:08:06 -0800

    Fix player conterr
    player moves and groves

--- P7SaucedoAndres2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
+++ P7SaucedoAndres2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
@@ -5,43 +5,23 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public InputAction LeftAction;
-    public InputAction MoveAction;
+  
     // Start is called before the first frame update
     void Start()
     {
-        LeftAction.Enable(); 
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = 0.0f; 
-        float vertical = 0.0f;
-
-        if(LeftAction.IsPressed())
-        {
-            horizontal = -1.0f;
-        }
-        else if (Keyboard.current.rightArrowKey.isPressed)
-        {
-            horizontal = 1.0f; 
-        }
-        Debug.Log(horizontal);
-
-        if (Keyboard.current.upArrowKey.isPressed)
-        {
-            vertical = 1.0f;
-        }
-        else if ( Keyboard.current.downArrowKey.isPressed)
-        {
-            vertical = -1.0f;
-        }
-        Debug.Log(vertical);
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
 
         Vector2 position = transform.position;
-        position.x = position.x + 5.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 5.0f * vertical * Time.deltaTime;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+
         transform.position = position; 
     }
 }
