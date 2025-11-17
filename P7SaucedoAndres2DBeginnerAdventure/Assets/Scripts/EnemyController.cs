30a023097ee1568529e5488b930e025a8c0243b8
Saucedo <732861@student.dusd.net>
2025-11-16 20:18:12 -0800

    Im tired, enemy code needs fixing
    Not sure why, but the enemy doesn't lower the health of the player. I need to fix that when I come back to this.
    
    Follow Mr.Quintero videos
    2DBA Character Controller 1-15
    World Design Tilemaps
    Decorating the World
    Interactions Blocking Movement
    Interactions Collectables
    Damage Zones and Enemies
    
    founded a shiny female ralts in PLZA
    
    6767676767676767676767676767676767676767676767676767676767676767676767676767676767676767676767676767676767676767676767676767676767676767676767676767676767676767
    Can you tell that i'm losing my mind?

---
+++ P7SaucedoAndres2DBeginnerAdventure/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.UIElements;
+
+public class EnemyController : MonoBehaviour
+{
+    public float speed;
+    public bool vertical;
+    public float changeTime = 3.0f; 
+
+    Rigidbody2D rigidbody2d;
+
+    float timer;
+    int direction = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        timer = changeTime;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+
+        if( timer < 0)
+        {
+            direction = -direction;
+            timer = changeTime;
+           
+        }
+    }
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
+
+        if (vertical)
+        {
+            position.y = position.y + speed * direction * Time.deltaTime;
+        }
+        else
+        {
+            position.x = position.x + speed * direction * Time.deltaTime; ;
+        }
+            //position.x = position.x + Time.deltaTime * speed;
+
+        rigidbody2d.MovePosition(position);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        PlayerController player = other.gameObject.GetComponent<PlayerController>();
+
+        if (player != null)
+        {
+            player.ChangeHealth(-1);
+        }
+    }
+}
--- P7SaucedoAndres2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
+++ P7SaucedoAndres2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
@@ -5,23 +5,75 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 3.0f; 
+    public int maxHealth = 5;
+    public float timeInvincible = 2;
+    public int health { get { return currentHealth; } }
+    int currentHealth; 
+
+    bool isInvincible;
+    float invincibleTimer;
+
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+
+    public InputAction MoveAction;
   
     // Start is called before the first frame update
     void Start()
     {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
         
+        //QualitySettings,vSyncCount = 0; 
+        //Application.targetFrameRate = 10;
+        //Can monkeys do backfilps?
+
+        MoveAction.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        Vector2 move = MoveAction.ReadValue<Vector2>();
+        Debug.Log(move);
+        Vector2 position = (Vector2)transform.position + move * speed * Time.deltaTime;
+        transform.position = position;
 
-        Vector2 position = transform.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if(invincibleTimer < 0)
+            {
+                isInvincible = false; 
+            }
 
-        transform.position = position; 
+        }
+    }
+    void FixedUpdate()
+    {
+        Vector2 move = MoveAction.ReadValue<Vector2>();
+        Debug.Log(move);
+        Vector2 position = (Vector2)transform.position + move * speed * Time.deltaTime;
+        transform.position = position;
+
+        Vector2 postion = rigidbody2d.position;
+        rigidbody2d.MovePosition(position);
+
+    }
+    public void ChangeHealth(int amount)
+    {
+        if(amount < 0)
+        {
+            if(isInvincible)
+            {
+                return;
+            }
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
